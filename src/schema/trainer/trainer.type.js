75becff6850be31bd21fd492d5bd3f8d0d16bf42
eretamero <eric2reta@gmail.com>
2018-06-27 17:02:59 +0200

    inherits ok

--- src/schema/trainer/trainer.type.js
+++ src/schema/trainer/trainer.type.js
@@ -1,25 +1,6 @@
 module.exports =
     `
-    type Trainer  { 
-        id:             Int
-        firstName:      String
-        lastName:       String
-        description:    String
-        file:           Int
-        email:          String
-        password:       String
-        createdAt:      String
-        updatedAt:      String
-        gender:         Gender
-        activated:      Boolean
-        username:       String
-        followers:      Int
-        followedBy:     Int
-        birthday:       String
-        lang:           String
-        facebook:       Int
-        google:         Int
-        instagram:      Int
+    type Trainer inherits User { 
         rating:         Float
         planCount:      Float
         planDelete:     Int
--- src/schema/type.js
+++ src/schema/type.js
@@ -1,11 +1,9 @@
 const { mergeTypes } = require('merge-graphql-schemas');
+const { transpileSchema } = require('graphql-s2s').graphqls2s
 
 const User = require('./user');
 const Trainer = require('./trainer');
 
-const types = [
-    User,
-    Trainer,
-];
+const types = [transpileSchema(User.concat(Trainer))];
 
-module.exports = mergeTypes(types, { all: true })
+module.exports = mergeTypes(types, { all: true });
